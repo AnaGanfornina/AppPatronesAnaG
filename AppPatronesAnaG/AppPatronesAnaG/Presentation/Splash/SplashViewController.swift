250fd10f833c9b0fa74832377f05ac1da112a5d3
Ana Ganfornina <anies.llamani@gmail.com>
2024-10-05 18:19:29 +0200

    initial commit whith assets

---
+++ AppPatronesAnaG/AppPatronesAnaG/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  AppPatronesAnaG
+//
+//  Created by Ana on 5/10/24.
+//
+
+import  UIKit
+
+final class SplashViewController: UIViewController{
+    init(){
+        super.init(nibName: "SplashView", bundle: Bundle(for:type(of: self)))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
